9db2b092619c8a8695f6f4984c74b5a304325689
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-05-17 17:36:02 -0400

    wrap all tests in testset and remove redundant set_schema tests

--- test/formula.jl
+++ test/formula.jl
@@ -1,38 +1,42 @@
-import StreamModels: sort_terms!
+@testset "Formulas" begin 
 
-@testset "Formula parsing" begin
+    import StreamModels: sort_terms!
 
-    @testset "Associative property" begin
-        @test parse!(:(a+(b+c))) == parse!(:(a+b+c))
-        @test parse!(:((a+b)+c)) == parse!(:(a+b+c))
-        @test parse!(:(a&(b&c))) == parse!(:(a&b&c))
-        @test parse!(:((a&b)&c)) == parse!(:(a&b&c))
+    @testset "Formula parsing" begin
+
+        @testset "Associative property" begin
+            @test parse!(:(a+(b+c))) == parse!(:(a+b+c))
+            @test parse!(:((a+b)+c)) == parse!(:(a+b+c))
+            @test parse!(:(a&(b&c))) == parse!(:(a&b&c))
+            @test parse!(:((a&b)&c)) == parse!(:(a&b&c))
+        end
+
+        @testset "Distributive property" begin
+            @test parse!(:(a & (b+c))) == parse!(:(a&b + a&c))
+            @test parse!(:((a+b) & c)) == parse!(:(a&c + b&c))
+            @test parse!(:((a+b) & (c+d))) == parse!(:(a&c + a&d + b&c + b&d))
+            @test parse!(:(a & (b+c) & d)) == parse!(:(a&b&d + a&c&d))
+        end
+        
+        @testset "Expand * to main effects + interactions" begin
+            @test parse!(:(a*b)) == parse!(:(a+b+a&b))
+            @test sort_terms!(parse!(:(a*b*c))) == parse!(:(a+b+c+a&b+a&c+b&c+a&b&c))
+            @test parse!(:(a + b*c)) == parse!(:(a + b + c + b&c))
+            @test parse!(:(a*b + c)) == parse!(:(a + b + a&b + c))
+        end
+
     end
 
-    @testset "Distributive property" begin
-        @test parse!(:(a & (b+c))) == parse!(:(a&b + a&c))
-        @test parse!(:((a+b) & c)) == parse!(:(a&c + b&c))
-        @test parse!(:((a+b) & (c+d))) == parse!(:(a&c + a&d + b&c + b&d))
-        @test parse!(:(a & (b+c) & d)) == parse!(:(a&b&d + a&c&d))
+    @testset "@formula" begin
+
+        @test (@formula(y ~ x)) == Formula(:y, :x)
+        @test (@formula(y ~ 1)) == Formula(:y, 1)
+        @test (@formula(y ~ 1 + x)) == Formula(:y, :(1+x))
+
+        @test (@formula( ~ x)) == Formula(nothing, :x)
+        @test (@formula( ~ 1)) == Formula(nothing, 1)
+        @test (@formula( ~ 1 + x)) == Formula(nothing, :(1+x))
+
     end
-    
-    @testset "Expand * to main effects + interactions" begin
-        @test parse!(:(a*b)) == parse!(:(a+b+a&b))
-        @test sort_terms!(parse!(:(a*b*c))) == parse!(:(a+b+c+a&b+a&c+b&c+a&b&c))
-        @test parse!(:(a + b*c)) == parse!(:(a + b + c + b&c))
-        @test parse!(:(a*b + c)) == parse!(:(a + b + a&b + c))
-    end
-
-end
-
-@testset "@formula" begin
-
-    @test (@formula(y ~ x)) == Formula(:y, :x)
-    @test (@formula(y ~ 1)) == Formula(:y, 1)
-    @test (@formula(y ~ 1 + x)) == Formula(:y, :(1+x))
-
-    @test (@formula( ~ x)) == Formula(nothing, :x)
-    @test (@formula( ~ 1)) == Formula(nothing, 1)
-    @test (@formula( ~ 1 + x)) == Formula(nothing, :(1+x))
 
 end
--- test/modelmatrix.jl
+++ test/modelmatrix.jl
@@ -1,41 +1,42 @@
-module TestModelMatrix
+@testset "Model matrix" begin
 
-using StreamModels
-using Base.Test
-using DataFrames
-using StreamModels
-using DataStreams
+    using StreamModels
+    using Base.Test
+    using DataFrames
+    using StreamModels
+    using DataStreams
 
-import StreamModels: @formula
+    import StreamModels: @formula
 
-source = DataFrame(a = collect(1:10),
-                   b = rand(10),
-                   c = repeat(["a", "b"], inner=5))
+    source = DataFrame(a = collect(1:10),
+                       b = rand(10),
+                       c = repeat(["a", "b"], inner=5))
 
-@testset "star expansion" begin 
-    f = @formula( ~ a*b*c)
+    @testset "star expansion" begin 
+        f = @formula( ~ a*b*c)
 
-    mm = modelmatrix(source, f)
+        mm = modelmatrix(source, f)
 
-    @test size(mm) == (10, 8)
-    @test mm[:,1] == source[:a]
-    @test mm[:,2] == source[:b]
-    @test mm[:,3] == [ones(5); zeros(5)]
-    @test mm[:,4] == [zeros(5); ones(5)]
-    @test mm[:,5] == source[:a] .* source[:b]
-    @test mm[:,6] == source[:a] .* [zeros(5); ones(5)]
-    @test mm[:,7] == source[:b] .* [zeros(5); ones(5)]
-    @test mm[:,8] == source[:a] .* source[:b] .* [zeros(5); ones(5)]
+        @test size(mm) == (10, 8)
+        @test mm[:,1] == source[:a]
+        @test mm[:,2] == source[:b]
+        @test mm[:,3] == [ones(5); zeros(5)]
+        @test mm[:,4] == [zeros(5); ones(5)]
+        @test mm[:,5] == source[:a] .* source[:b]
+        @test mm[:,6] == source[:a] .* [zeros(5); ones(5)]
+        @test mm[:,7] == source[:b] .* [zeros(5); ones(5)]
+        @test mm[:,8] == source[:a] .* source[:b] .* [zeros(5); ones(5)]
+    end
+
+    @testset "arbitrary functions" begin
+
+        @test modelmatrix(source, @formula(~ log(a)))[:,1] == log.(float(source[:a]))
+        @test modelmatrix(source, @formula(~ log(1+a)))[:,1] == log.(1.+float(source[:a]))
+        @test modelmatrix(source, @formula(~ 1 + log(a))) == [ones(10) log.(float(source[:a]))]
+        @test modelmatrix(source, @formula(~ log(a+b)))[:,1] == log.(source[:a].+source[:b])
+        ## @test modelmatrix(source, @formula(~ log(a*b)))[:,1] == log.(source[:a].*source[:b])
+
+    end
+
 end
 
-@testset "arbitrary functions" begin
-
-    @test modelmatrix(source, @formula(~ log(a)))[:,1] == log.(float(source[:a]))
-    @test modelmatrix(source, @formula(~ log(1+a)))[:,1] == log.(1.+float(source[:a]))
-    @test modelmatrix(source, @formula(~ 1 + log(a))) == [ones(10) log.(float(source[:a]))]
-    @test modelmatrix(source, @formula(~ log(a+b)))[:,1] == log.(source[:a].+source[:b])
-    ## @test modelmatrix(source, @formula(~ log(a*b)))[:,1] == log.(source[:a].*source[:b])
-
-end
-
-end
