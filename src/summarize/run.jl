c47e5e8137f6850b6676f244c766e44e541a224a
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-11-07 20:09:11 -0500

    summarize with multiple formulae

--- src/summarize/run.jl
+++ src/summarize/run.jl
@@ -7,11 +7,12 @@ Summaries are stored in the metadata of the schema for source.  This is mediated
 by dispatching on the types of the terms and the types of the data via 
 `make_summarizer`.
 """
-summarize(source::Data.Table, f::Formula) = summarize!(Data.schema(source), source, f)
+summarize(source::Data.Table, fs::Formula...) = summarize!(Data.schema(source), source, f...)
 
-function summarize!(sch::Data.Schema, source::Data.Table, f::Formula)
+function summarize!(sch::Data.Schema, source::Data.Table, fs::Formula...)
+    terms = vcat(f.terms for f in fs)
     types = Dict(Symbol(k) => Data.types(sch)[sch[k]] for k in Data.header(sch))
-    summarizers = unique(reduce(vcat, [], make_summarizer(t, types) for t in f.terms))
+    summarizers = unique(reduce(vcat, [], make_summarizer(t, types) for t in terms))
     isempty(summarizers) && return sch
     for nt in RowIterator(source)
         for s in summarizers
